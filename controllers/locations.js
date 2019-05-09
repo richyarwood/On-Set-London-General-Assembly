f93cb2aca7a8ac8597bcc8578a2dce944f6d1038
Samantha Kellow <sammii.h@icloud.com>
2019-05-09 15:18:30 +0100

    ammended  to log

--- controllers/locations.js
+++ controllers/locations.js
@@ -1,15 +1,15 @@
 const Location = require('../models/Location')
 
-function indexRoute(req, res, next) {
+function indexRoute(req, res, err) {
   Location.find()
     .then(locations => res.json(locations))
-    .catch(next)
+    .catch(console.log(err))
 }
 
-function showRoute(req, res, next) {
+function showRoute(req, res, err) {
   Location.findById(req.params.id)
     .then(location => res.json(location))
-    .catch(next)
+    .catch(console.log(err))
 }
 
 module.exports = {
