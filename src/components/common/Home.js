93741f1d0065a05d399df1eff6ba4977752495ef
Richard Yarwood <richyarwood@gmail.com>
2019-05-09 14:00:55 +0100

    Folder structure in place and ReactMapbox GL installed

---
+++ src/components/common/Home.js
@@ -0,0 +1,4 @@
+// This is the base template
+// Components are referenced within this.
+// It is a classical component as we need a constructor
+// Renders the map and includes the sidebar, add location button, register/login
