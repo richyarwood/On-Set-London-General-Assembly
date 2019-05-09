5ed76ec3e8f1ff2dee116ed4071fd3fa441ac570
Samantha Kellow <sammii.h@icloud.com>
2019-05-09 15:32:30 +0100

    basic app.js set up, to route '/', importing Home component

--- src/app.js
+++ src/app.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import { HashRouter as Router, Switch, Route } from 'react-router-dom'
+
+import Home from './component/common/Home'
+
 class App extends React.Component{
   render(){
     return(
-      <h1>Hello World</h1>
+      <Router>
+        <Switch>
+          <Route path="/" component={Home} />
+        </Switch>
+      </Router>
     )
   }
 }
