12f5d7af46c05d421d82bd369e8d08e724cd704c
Samantha Kellow <sammii.h@icloud.com>
2019-05-09 21:40:42 +0100

    altered react index component from function to classical to resolve handleClick() and this state error

--- src/components/location/LocationIndex.js
+++ src/components/location/LocationIndex.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import axios from 'axios'
 
 import ReactMapboxGl, { Marker } from 'react-mapbox-gl'
 
@@ -6,26 +7,56 @@ const Map = ReactMapboxGl({
   accessToken: process.env.MAPBOX_API_TOKEN
 })
 
-const LocationIndex = () => {
-  <div className="location">
-    <Map
-      style='mapbox://styles/mapbox/streets-v9'
-      center = {[ this.state.center.lat, this.state.center.long ]}
-      zoom = {[14]}
-      containerStyle={{
-        height: '100vh',
-        width: '100vw'
-      }}>
+class LocationIndex extends React.Component {
 
-      {this.state.data.map(marker =>
-        <Marker key={marker.id}
-          coordinates={[marker.coordinates.lat, marker.coordinates.long]}
-          anchor="bottom">
-          <img src='https://i.pinimg.com/originals/30/98/49/309849c5815761081926477e5e872f1e.png' width='30px'/>
-        </Marker>
-      )}
-    </Map>
-  </div>
+  constructor(){
+    super()
+
+    this.state = {
+      data: [],
+      locations: [],
+      center: {
+        lat: -0.070839,
+        long: 51.515619
+      }
+    }
+
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick(e){
+    const data = e.target.id.split(',')
+    this.setState( { center: { lat: data[0], long: data[1] } } )
+  }
+
+  componentDidMount() {
+    axios('/locations')
+      .then(res => this.setState({ locations: res.data }))
+  }
+
+  render() {
+    return (
+      <div className="location">
+        <Map
+          style='mapbox://styles/mapbox/streets-v9'
+          center = {[ this.state.center.lat, this.state.center.long ]}
+          zoom = {[14]}
+          containerStyle={{
+            height: '100vh',
+            width: '100vw'
+          }}>
+
+          {this.state.data.map(marker =>
+            <Marker key={marker.id}
+              coordinates={[marker.coordinates.lat, marker.coordinates.long]}
+              anchor="bottom">
+              <img src='https://i.pinimg.com/originals/30/98/49/309849c5815761081926477e5e872f1e.png' width='30px'/>
+            </Marker>
+          )}
+        </Map>
+      </div>
+    )
+  }
 }
 
 export default LocationIndex
