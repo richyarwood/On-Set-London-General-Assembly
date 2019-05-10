46bb4954612aed0a520104fe163c447b61ac6802
Violeta Paez Armando <violetapaezarmando@gmail.com>
2019-05-10 10:10:19 +0100

    Moved scene notes from film to location schema

--- controllers/locations.js
+++ controllers/locations.js
@@ -1,7 +1,9 @@
 const Location = require('../models/Location')
+require('../models/Film')
 
 function indexRoute(req, res, err) {
   Location.find()
+    .populate('films sceneNotes.film')
     .then(locations => res.json(locations))
     .catch(console.log(err))
 }
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose')
 
 const locationsController = require('./controllers/locations')
 const app = express()
+mongoose.connect('mongodb://localhost:27017/on-set-london')
 
 app.get('/', (req, res) => res.json({ message: 'Welcome to On Set' }))
 app.get('/locations', locationsController.index)
@@ -11,6 +12,5 @@ app.get('/locations', locationsController.index)
 
 app.use(bodyParser.json())
 
-mongoose.connect('mongodb://localhost:27017/on-set-london')
 
 app.listen(4000, () => console.log('Listening on port 4000'))
