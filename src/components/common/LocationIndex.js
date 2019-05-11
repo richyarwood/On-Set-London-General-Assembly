a3f36ae16c2f5612479621319059717a22ba38e2
Samantha Kellow <sammii.h@icloud.com>
2019-05-11 12:31:39 +0100

    basic toggle function working

--- src/components/common/LocationIndex.js
+++ src/components/common/LocationIndex.js
@@ -10,8 +10,16 @@ class LocationIndex extends React.Component {
       center: {
         lat: '',
         long: ''
-      }
+      },
+      activeIndex: true
     }
+
+    this.toggleActiveLocation = this.toggleActiveLocation.bind(this)
+  }
+
+  toggleActiveLocation() {
+    const currentState = this.state.activeIndex
+    this.setState({ activeIndex: !currentState })
   }
 
   render() {
@@ -21,19 +29,31 @@ class LocationIndex extends React.Component {
     if (!this.props) return <h1>Loading...</h1>
     return (
       <div>
-        {this.props.data.map(location =>
-          <div key={location._id}>
-            <div className="title is-4">{location.name}</div>
-            <div className="location-image"
-              data-lat={location.coordinates.lat}
-              data-long={location.coordinates.long}
-              style={{ backgroundImage: `url(${location.image})` }} onClick={this.props.handleClick}>
+        <div
+          className={this.state.activeIndex ? null : 'hidden'}
+          onClick={this.toggleActiveLocation} >
+          {this.props.data.map(location =>
+            <div key={location._id}>
+              <div className="title is-4">{location.name}</div>
+              <div className="location-image"
+                data-lat={location.coordinates.lat}
+                data-long={location.coordinates.long}
+                style={{ backgroundImage: `url(${location.image})` }} onClick={this.props.handleClick}>
+              </div>
+              {location.films.map(film =>
+                <div key={film._id} className="film-title is-1">{film.title}</div>)}
+              <hr />
             </div>
-            {location.films.map(film =>
-              <div key={film._id} className="film-title is-1">{film.title}</div>)}
-            <hr />
-          </div>
-        )}
+          )}
+        </div>
+
+        <div
+          className={!this.state.activeIndex ? null : 'hidden'}
+          onClick={this.toggleActiveLocation} >
+          <h1>Toggled</h1>
+        </div>
+
+
       </div>
     )
   }
