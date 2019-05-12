ba57a9a415e906ea861d213f1fbbaf1f9f3f4a0d
Richard Yarwood <richyarwood@gmail.com>
2019-05-12 12:36:14 +0100

    Updated heroku settings

--- config/environment.js
+++ config/environment.js
@@ -1,5 +1,6 @@
 const secret = process.env.SECRET || 'Zge{T*g._&;(gCaQ2mcn=-mR'
 const dbUri = process.env.MONGODB_URI || 'mongodb://localhost:27017/on-set-london'
 const port = process.env.PORT || 4000
+const mapAPI = process.env.MAPBOX_API_TOKEN
 
-module.exports = { secret, dbUri, port }
+module.exports = { secret, dbUri, port, mapAPI }
