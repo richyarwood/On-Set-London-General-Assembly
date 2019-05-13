febd2df6843b77ff2cd6715a6730c35f5388d438
Richard Yarwood <richyarwood@gmail.com>
2019-05-13 11:50:30 +0100

    Updated post with secureRoute

--- config/routes.js
+++ config/routes.js
@@ -3,16 +3,17 @@ const router = require('express').Router()
 const locationsController = require('../controllers/locations')
 const filmsController = require('../controllers/films')
 const authenticationController = require('../controllers/auth')
+const secureRoute = require('../lib/secureRoute')
 
 router.get('/', (req, res) => res.json({ message: 'Welcome to On Set London' }))
 
 router.get('/locations', locationsController.index)
 router.get('/locations/:id', locationsController.show)
-router.post('/locations/', locationsController.create)
+router.post('/locations/', secureRoute, locationsController.create)
 
 router.get('/films', filmsController.index)
 router.get('/films/:id', filmsController.show)
-router.post('/films/', filmsController.create)
+router.post('/films/', secureRoute, filmsController.create)
 
 router.post('/register', authenticationController.register)
 router.post('/login', authenticationController.login)
