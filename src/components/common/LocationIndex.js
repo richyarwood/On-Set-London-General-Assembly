a60cbc4e77aeecd56f2f123c7add7b0413280416
Samantha Kellow <sammii.h@icloud.com>
2019-05-14 14:07:40 +0100

    filtering locations now all working

---
+++ src/components/common/LocationIndex.js
@@ -0,0 +1,89 @@
+import React from 'react'
+
+class LocationIndex extends React.Component {
+
+  constructor(props){
+    super(props)
+
+    this.state = {
+      activeLocation: null,
+      sort: 'name|asc'
+    }
+  }
+
+  toggleActiveLocation(location){
+    if(location === this.state.activeLocation) this.setState({ activeLocation: null })
+    else this.setState({ activeLocation: location })
+  }
+
+  sortedLocations() {
+    return this.props.data.sort((a, b) => {
+      if (a.name === b.name) return 0
+      return a.name < b.name ? -1 : 1
+    })
+  }
+
+  // set state search: ''
+  // set state.catergoy
+  // if empty (cat.length[0]), show all
+  // filter on entire index array
+  // prod.cat === value of input return it
+  // // location =>
+  // return location.region
+
+  // make a func filter location, first sort by called sort func, then flter and map over filter results in render
+
+  render() {
+    console.log([ ...this.props.data ])
+    console.log('locations-2', this.props.data)
+    if (!this.props) return <h1>Loading...</h1>
+    console.log(this.props.data)
+    return (
+      <div>
+        {this.sortedLocations().map(location =>
+          <div key={location._id} >
+            {// -----ITEMS ARE ALWAYS VISIBILE-----}
+            }
+            <div
+              onClick={() => this.toggleActiveLocation(location)}
+            >
+              <div className="title is-4">{location.name}</div>
+              <div className="subtitle is-6">{location.areaOfLondon}</div>
+              <div className="location-image"
+                data-lat={location.coordinates.lat}
+                data-long={location.coordinates.long}
+                onClick={this.props.handleClick}
+                style={{ backgroundImage: `url(${location.image})` }} >
+              </div>
+              <div className="is-size-6"> {`${location.streetAddress}, ${location.postCode}`}
+              </div>
+            </div>
+
+            {// -----ITEMS ARE HIDDEN ON LOAD AND APPEAR ON CLICK-----
+            }
+            <div
+              className={`locationShow${this.state.activeLocation !== location ? '' : ' show' }`}>
+              <hr />
+              <div className="subtitle is-size-6">Films and notes</div>
+              {location.sceneNotes.map(note =>
+                <div key={note._id} className="note-wrapper">
+                  <div className="columns">
+                    <div className="column"><img src="https://upload.wikimedia.org/wikipedia/en/thumb/9/98/Lara_Croft_film.jpg/220px-Lara_Croft_film.jpg" /></div>
+                    <div className="column is-four-fifths">
+                      <div className="subtitle is-size-5 has-text-weight-bold indexTitle">{note.film.title}</div>
+                      <p className="is-5">{note.text}</p>
+                    </div>
+                  </div>
+                </div>
+              )}
+            </div>
+            <hr className="dark-hr" />
+          </div>
+        )}
+      </div>
+    )
+  }
+}
+
+
+export default LocationIndex
