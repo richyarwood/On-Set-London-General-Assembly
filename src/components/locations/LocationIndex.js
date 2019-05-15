ade8980cfb3355de52229f659656f401e3d40eac
Samantha Kellow <sammii.h@icloud.com>
2019-05-15 19:47:26 +0100

    padding and other styling adjustments make on locationIndex location containers at mobile breakpoint

--- src/components/locations/LocationIndex.js
+++ src/components/locations/LocationIndex.js
@@ -55,12 +55,13 @@ class LocationIndex extends React.Component {
   render() {
     if (!this.props) return <h1>Loading...</h1>
     return (
-      <div>
+      <div className="locationsIndex">
         <Select
           defaultValue={areasOfLondon[0]}
           options={areasOfLondon}
           name="areaOfLondon"
           onChange={this.handleChange}
+          className="filterSelect"
         />
         <hr />
         {this.filteredLocations().map(location =>
@@ -68,6 +69,7 @@ class LocationIndex extends React.Component {
             {// -----ITEMS ARE ALWAYS VISIBILE-----}
             }
             <div
+              className="locationsContainer"
               onClick={() => this.toggleActiveLocation(location)}
             >
               <div className="title is-4">{location.name}</div>
@@ -78,7 +80,7 @@ class LocationIndex extends React.Component {
                 onClick={this.props.handleLocationClick}
                 style={{ backgroundImage: `url(${location.image})` }} >
               </div>
-              <div className="is-size-6"> {`${location.streetAddress}, ${location.postCode}`}
+              <div className="address is-size-6"> {`${location.streetAddress}, ${location.postCode}`}
               </div>
             </div>
 
